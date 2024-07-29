fb77dd65b9021af935a92f965f28abbbba4a494e
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-29 20:21:21 -0300

    Feat: Criar um loop no game usando nano.Time para chamar a cada 1/60 segundos os métodos

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,14 +3,39 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     public static final int WIDTH = 1100;
     public static final int HEIGHT = 800;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.BLACK);
+    }
+
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if
+        }
+
     }
 
     private void update() {
