f8c149eb0ce34f2200fffc8b0ae63b39ef7e42ca
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-29 20:06:45 -0300

    Feat: Criar Classe GamePanel para definir e alterar a tela do jogo

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,7 +1,16 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+
+    public static final int WIDTH = 1100;
+    public static final int HEIGHT = 800;
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setBackground(Color.BLACK);
+    }
 
 }
