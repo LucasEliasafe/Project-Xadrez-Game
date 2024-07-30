0621ac6f5d038c6b4055e6a741c83093e3439096
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-29 21:15:18 -0300

    Feat: criar classe para as peças do jogo

---
+++ src/piece/Piece.java
@@ -0,0 +1,12 @@
+package piece;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+
+public class Piece {
+
+    public BufferedImage image;
+    public int x, y;
+    public int col, row, preCol, preRow;
+    public int color;
+}
