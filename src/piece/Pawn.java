b1032bf2b592334b965e76ad9620a6fa3f391494
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-30 21:28:10 -0300

    Feat:Criar Classe Pawn para definir quando o arquivo for b= preto, w=branco, as cores das peças

--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -1,2 +1,16 @@
-package piece;public class Pawn {
+package piece;
+
+import main.GamePanel;
+
+public class Pawn extends Piece {
+
+    public Pawn(int color, int col, int row) {
+        super(color, col, row);
+
+    if(color == GamePanel.WHITE) {
+        image = getImage("/piece/w-pawn");
+    }else {
+        image = getImage("/piece/b-pawn");
+        }
+    }
 }
