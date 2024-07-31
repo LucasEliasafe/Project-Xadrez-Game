952e1d236a4de1f89aebc556417a8eb1bc4dff8f
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-31 20:47:38 -0300

    Feat: Após importar as imagens para o tabuleiro, criar classe para o movimento

---
+++ src/main/Mouse.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Mouse extends MouseAdapter {
+   public int x, y;
+           public boolean  pressed;
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged (MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
