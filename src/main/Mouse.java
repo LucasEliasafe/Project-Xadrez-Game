dfb19640083586baa992a86f0a6e1f5ac499207d
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-31 21:13:03 -0300

    Feat: Criar métodos para os movimentos e linkar com cada peça

--- src/main/Mouse.java
+++ src/main/Mouse.java
@@ -9,21 +9,23 @@ public class Mouse extends MouseAdapter {
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        pressed = true;
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        pressed = false;
     }
 
     @Override
     public void mouseDragged (MouseEvent e) {
-
+        x = e.getX();
+        y = e.getY();
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-
+        x = e.getX();
+        y = e.getY();
     }
 }
